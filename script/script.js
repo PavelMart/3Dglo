a35cc9c8a7421cc7e9fb87861641c0e20d900670
Pavel Martakov <martackovp@yandex.ru>
2021-04-30 13:40:14 +0300

    Сначала сделал калькулятор, чтобы потом доделать 22 урок

--- script/script.js
+++ script/script.js
@@ -351,5 +351,71 @@ window.addEventListener('DOMContentLoaded', () => {
 	};
 	slider();
 
+	// calculator
+	const calculator = (price) => {
+		const calcBlock = document.querySelector('.calc-block'),
+			calcType = document.querySelector('.calc-type'),
+			calcSquare = document.querySelector('.calc-square'),
+			calcCount = document.querySelector('.calc-count'),
+			calcDay = document.querySelector('.calc-day'),
+			total = document.getElementById('total');
+
+		// allowed simbols 
+		calcBlock.addEventListener('keypress', event => {
+			const key = event.key;
+			const target = event.target;
+			const notNumbers = /\D/;
+
+			if (target.tagName === 'INPUT') {
+				if (key.match(notNumbers)) {
+					event.preventDefault();
+				}
+			} 
+		}); 
+		
+		// calculation
+		calcBlock.addEventListener('change', () => {
+			let sum = 0,
+				countRoom = 1,
+				countDay = 1;
+
+			if (calcCount.value && calcCount.value > 1) {
+				countRoom += (calcCount.value - 1) / 10;
+			} 
+
+			if (calcDay.value && calcDay.value < 5) {
+				countDay = 2;
+			} else if (calcDay.value && calcDay.value < 10 ) {
+				countDay = 1.5;
+			}
+
+			if (calcType.value && calcSquare.value) {
+				sum = Math.floor(price * calcType.value * calcSquare.value * countRoom * countDay);
+			}
+
+			total.textContent = sum;
+		});
+	};
+	calculator(100);
+
+	// Change Photo of command
+	const changePhoto = () => {
+		const commandPhotos = document.querySelectorAll('.command__photo');
+
+		commandPhotos.forEach( item => {
+			const src = item.src;
+			const datasetImg = item.dataset.img;
+
+			item.addEventListener('mouseover', () => {
+				item.src = datasetImg;
+			});
+			item.addEventListener('mouseout', () => {
+				item.src = src;
+			});
+		});
+	};
+	changePhoto();
+
+
 });
 
